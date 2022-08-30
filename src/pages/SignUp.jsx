cfbfb491293dfe0e4c134e82110139265c53c2f8
Rishika Jain <77011730+rjrishika@users.noreply.github.com>
2022-08-30 17:27:53 +0530

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,12 +1,13 @@
 import React,{useState} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
-import {getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
-import {setDoc, doc, serverTimestamp} from 'firebase/firestore'
+import {Link, useNavigate,useLocation} from 'react-router-dom'
+import {getAuth, createUserWithEmailAndPassword, updateProfile, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import {setDoc,getDoc, doc, serverTimestamp} from 'firebase/firestore'
 import {db} from '../firebase.config'
 import {toast} from 'react-toastify'
 import {ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 import OAuth from '../components/OAuth'
+import googleIcon from '../assets/svg/googleIcon.svg'
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false)
@@ -18,6 +19,7 @@ function SignUp() {
   const {name,email, password} = formData
 
   const navigate = useNavigate()
+  const location = useLocation()
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -25,6 +27,28 @@ function SignUp() {
       [e.target.id]: e.target.value,
     }) )
   }
+  const onGoogleClick = async() => {
+    try {
+        const auth = getAuth()
+        const provider = new GoogleAuthProvider()
+        const result = await signInWithPopup(auth, provider)
+        const user = result.user
+
+        const docRef = doc(db, 'users', user.uid)
+        const docSnap = await getDoc(docRef)
+
+        if(!docSnap.exists()){
+           await setDoc(doc(db, 'users', user.uid), {
+               name: user.displayName,
+               email: user.email,
+               timestamp: serverTimestamp()
+           })
+        }
+        navigate('/')
+    } catch (error) {
+        toast.error('Could not authorize with Google')
+    }
+ }
 
   const onSubmit = async (e) => {
     e.preventDefault()
@@ -54,50 +78,66 @@ function SignUp() {
   }
 
   return (
-    <>
-      <div className="pageContainer">
-        <header>
-          <p className="pageHeader">Welcome</p>
-        </header>
+  
+      <>
+<div className='index'>
+  <div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Sign Up</h3>
+				<div class="d-flex justify-content-end social_icon">
+					<span><i class="fab fa-facebook-square"></i></span>
+					<span><i class="fab fa-google-plus-square" onClick={onGoogleClick}></i></span>
+					<span><i class="fab fa-twitter-square"></i></span>
+				</div>
+			</div>
+			<div class="card-body">
+				<form onSubmit={onSubmit}>
+        <div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
 
-        <main>
-          <form onSubmit={onSubmit} >
+						<input type="text" class="form-control" id='name' placeholder="username" value={name} onChange={onChange} />
+						
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fa fa-envelope"></i></span>
+						</div>
 
-          <input type="text" className="nameInput" placeholder='Name' id='name' value={name} onChange={onChange} />
-
-          <input type="email" className="emailInput" placeholder='Email' id='email' value={email} onChange={onChange} />
-
-
-            <div className="passwordInputDiv">
-              <input type={showPassword ? 'text' : 'password'} className="passwordInput" placeholder='Password' id="password" value={password} onChange={onChange} />
-
+						<input type="text" class="form-control" id='email' placeholder="email" value={email} onChange={onChange} />
+						
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type={showPassword ? 'text' : 'password'} class="form-control" id="password" placeholder="password" value={password} onChange={onChange} />
             <img src={visibilityIcon} alt="show password" className="showPassword" onClick={()=> setShowPassword((prevState)=>!prevState)} />
-
-
-            </div>
-            <Link to='/forgot-password' className='forgotPasswordLink' >
-              Forgot Password
-            </Link>
-
-            <div className="signUpBar">
-              <p className="signUpText">
-                Sign Up
-              </p>
-              <button className="signUpButton">
-                <ArrowRightIcon fill='#ffffff' width='34px' height='34px'/>
-              </button>
-            </div>
-
-          </form>
-
-          <OAuth />
-
-          <Link to='/sign-in'  className='registerLink'>
-            Sign In Instead
-          </Link>
-        </main>
-      </div>
-    </>
+					</div>
+					<div class="row align-items-center remember">
+						<input type="checkbox"/>Remember Me
+					</div>
+					<div class="form-group">
+						<input type="submit" value="Sign Up" class="btn float-right login_btn"/>
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center links">
+					Don't have an account?<a className='forgetPass' href="/sign-in">Sign In</a>
+				</div>
+				<div class="d-flex justify-content-center">
+					<a className='forgetPass' href="/forgot-password">Forgot your password?</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
+</>
   )
 }
 
