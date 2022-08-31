d2ce4dea907f6314f78788ae86671dbf22941d78
Rishika Jain <77011730+rjrishika@users.noreply.github.com>
2022-08-31 11:16:10 +0530

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,26 +1,42 @@
 import { Button } from 'bootstrap'
 import React,{useState,useEffect} from 'react'
 import {useParams} from 'react-router-dom'
-import {collection, query, where, orderBy, limit,startAfter, getDocs } from 'firebase/firestore'
+import {collection, query, where, orderBy, limit,startAfter, getDocs, loadBundle } from 'firebase/firestore'
 import {db} from "../firebase.config"
 import {toast} from 'react-toastify'
 
 function Search() {
+  const [listings, setListings] = useState([])
+  const [loading,setLoading] = useState(true)
+  const [search, setSearch] = useState('')
+  
+  const params = useParams()
+
+
+  const handleSearch=async() => {
+    if(search){
+      const response=db.collection('listings');
+      const data=await response.get();
+      console.log(data);
+    
+      const searched = data.filter(
+       (listings)=> listings.name.toLowerCase().includes(search)
+       ||listings.location.toLowerCase().includes(search)
+     );
+     setListings(searched)
+    }
+ }
+  
   
   return (
        <div>
          <form>
-            <input type="text" className="searchInput" placeholder='Search' id='text' />
+           
             <div className="signInBar">
-              <button className="signInText btn btn-dark" style={{position:"relative",bottom:70 , borderRadius:40,fontSize:20}}>
+             <input type="text" className="searchInput" placeholder='Search' id='text' onChange={e => setSearch(e.target.value)} /> 
+              <button className="signInText btn btn-dark" style={{position:"relative",bottom:20,left:6 , borderRadius:40,fontSize:20}} onClick={handleSearch}>
                 Search
               </button>
-            <div classname="ui search">
-              <div classname="ui icon input">
-                <input type="text" placeholder="Search Gyms" classname="prompt"/>
-                  <i className="search icon"></i>
-              </div>
-              </div>
             </div>
          </form>
        </div>
